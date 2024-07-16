7ae681e34eedf0eb44aa069ccb0b724017c1cf9e
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-16 18:36:58 -0400

    Commit #6

--- Assets/starBlink.cs
+++ Assets/starBlink.cs
@@ -6,17 +6,45 @@ using UnityEngine.Rendering.Universal;
 
 public class starBlink : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     public Light2D starlight;
+    public float blinkInterval = 1.0f; 
+    public float lightOnDuration = 3.5f;
+
+    private float timer = 0.0f;
+
     void Start()
     {
         starlight = GetComponent<Light2D>();
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        timer += Time.deltaTime;
+
+        if (timer > blinkInterval)
+        {
+  
+            timer = 0.0f;
+            starlight.enabled = !starlight.enabled;
+            StartCoroutine(BlinkLightCoroutine());
+            //FIX THIS LATER
+        }
     }
+
+
+    IEnumerator BlinkLightCoroutine()
+    {
+         float targetIntensity = starlight.enabled ? 0.7f : 1.0f;
+         float startIntensity = starlight.intensity;
+         float elapsedTime = 0.0f;
+    
+         while (elapsedTime < lightOnDuration)
+         {
+             starlight.intensity = Mathf.Lerp(startIntensity, targetIntensity, elapsedTime / lightOnDuration);
+             elapsedTime += Time.deltaTime;
+             yield return null;
+         }
+     }
 }
