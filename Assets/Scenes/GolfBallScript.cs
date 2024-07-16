5a576b8e06e31261000e49ee84c96234d3951687
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-16 09:55:37 -0400

    Commit #2

--- Assets/Scenes/GolfBallScript.cs
+++ Assets/Scenes/GolfBallScript.cs
@@ -9,28 +9,34 @@ public class GolfBallScript : MonoBehaviour
 
     public bool isMoving;
     public bool isAiming;
+    public bool isWon;
+
     public Vector2 aimDirection;
     public float power;
+
+    public int shots;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         isMoving = false;
-        isAiming = true;
+        isAiming = false;
+        isWon = false;
+
         power = 0f;
+        shots = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isMoving)
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if ((Vector2.Distance(transform.position, mousePos)<1f)&&Input.GetMouseButtonDown(0) && !isMoving)
         {
             rb.velocity = Vector2.zero;
             isAiming = true;
         }
-        if (Input.GetMouseButton(0) && !isMoving)
+        if (Input.GetMouseButton(0) && isAiming)
         {
-
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             aimDirection = (mousePos - (Vector2)transform.position).normalized;
             power = Vector2.Distance((Vector2) transform.position, mousePos);
         }
