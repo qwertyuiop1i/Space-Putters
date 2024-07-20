54d5af9da8558685297b19be327c40c8558c7fb7
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-20 18:53:18 -0400

    Commit #40

--- Assets/fanBehavior.cs
+++ Assets/fanBehavior.cs
@@ -6,18 +6,22 @@ public class fanBehavior : MonoBehaviour
 {
     public float forceStrength = 10f;
     public float radius = 5f;
-   
 
+    public ParticleSystem ps;
 
-
-
+    public void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        var main = ps.main;
+        main.startSpeed = 0.2f * forceStrength;
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
         if (rb!=null)
         {
-            rb.AddForce(transform.up*forceStrength*Time.deltaTime);
+            rb.AddForce(transform.up*forceStrength/(0.1f+0.01f*Vector2.Distance(transform.position,collision.transform.position))*Time.deltaTime);
             //Debug.Log("ge");
         }
     }
