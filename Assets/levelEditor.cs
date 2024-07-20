bd17f97425b0024106c8a92bd46ae55534b028fc
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-20 19:54:28 -0400

    Commit #41

--- Assets/levelEditor.cs
+++ Assets/levelEditor.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class levelEditor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject selected;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (selected)
+            {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Debug.Log("Placed");
+
+                float gridSize = 1f;
+                float snappedX = Mathf.Round(mousePos.x / gridSize) * gridSize;
+                float snappedY = Mathf.Round(mousePos.y / gridSize) * gridSize;
+                Vector2 snappedPosition = new Vector3(snappedX, snappedY,0f);
+
+                Instantiate(selected, snappedPosition,Quaternion.identity);
+
+            }
+        }
+    }
+    public void select(GameObject selection)
+    {
+        selected = selection;
     }
 }
