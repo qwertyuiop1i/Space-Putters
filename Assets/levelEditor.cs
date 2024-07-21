543bfc75d02b29a8717c9339ba3deba95e394cc0
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-21 10:56:29 -0400

    Commit #45

--- Assets/levelEditor.cs
+++ Assets/levelEditor.cs
@@ -16,10 +16,21 @@ public class levelEditor : MonoBehaviour
     }
     void Update()
     {
+        transform.localScale = new Vector2(1,1)*size;
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float gridSize = 1f;
-        float snappedX = Mathf.Round(mousePos.x / gridSize) * gridSize;
-        float snappedY = Mathf.Round(mousePos.y / gridSize) * gridSize;
+        float snappedX = Mathf.Round(mousePos.x / gridSize) * gridSize+0.5f;
+        float snappedY = Mathf.Round(mousePos.y / gridSize) * gridSize+0.5f;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            size += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            size -= 0.05f;
+        }
+        size = Mathf.Clamp(size, 0.1f, 4f);
 
 
         transform.position = new Vector2(snappedX, snappedY);
@@ -36,7 +47,8 @@ public class levelEditor : MonoBehaviour
             {
                 Debug.Log("Placed");
                 Vector2 snappedPosition = new Vector3(snappedX, snappedY, 0f);
-                Instantiate(selected, snappedPosition, Quaternion.identity);
+                GameObject instance=Instantiate(selected, snappedPosition, Quaternion.identity);
+                instance.transform.localScale = size * new Vector2(1, 1);
             }
         }
     }
