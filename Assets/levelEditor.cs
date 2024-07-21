db03797fb24ff9aeb5680689991ecf0161e4d38c
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-20 23:31:00 -0400

    Commit #44
    In process of trying to fix level editor rendering bug

--- Assets/levelEditor.cs
+++ Assets/levelEditor.cs
@@ -5,44 +5,48 @@ using UnityEngine;
 public class levelEditor : MonoBehaviour
 {
     public GameObject selected;
-    //public GameObject[]
-
-    public SpriteRenderer sr;
+    public SpriteRenderer previewSprite;
     public float size;
     public float strength;
     public float rot;
 
-
+    private void Start()
+    {
+        previewSprite = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float gridSize = 1f;
         float snappedX = Mathf.Round(mousePos.x / gridSize) * gridSize;
         float snappedY = Mathf.Round(mousePos.y / gridSize) * gridSize;
+
+
+        transform.position = new Vector2(snappedX, snappedY);
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+        previewSprite.sprite = selected.GetComponent<SpriteRenderer>().sprite;
         
-        transform.position = new Vector2(snappedX,snappedY);
+
+  
+        previewSprite.color = new Color(1f, 1f, 1f, strength);
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (selected&&selected.name!= "empty")
+            if (selected && selected.name != "empty")
             {
-                
                 Debug.Log("Placed");
-
-                
-                Vector2 snappedPosition = new Vector3(snappedX, snappedY,0f);
-                
-                Instantiate(selected, snappedPosition,Quaternion.identity);
-
+                Vector2 snappedPosition = new Vector3(snappedX, snappedY, 0f);
+                Instantiate(selected, snappedPosition, Quaternion.identity);
             }
         }
     }
-    public void select(GameObject selection=null)
+
+    public void select(GameObject selection = null)
     {
         if (selection)
         {
             selected = selection;
-
+            previewSprite.sprite = selected.GetComponent<SpriteRenderer>().sprite; 
         }
-
     }
 }
